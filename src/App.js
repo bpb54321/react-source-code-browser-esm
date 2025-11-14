8dd3571f9041b1208d364b7b2fdad82539cb8206
Brian Blosser <bpb54321@gmail.com>
2025-11-14 16:01:20 -0500

    Set up example with two counters.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import { useEffect, useState } from "react";
+import { Counter } from "./Counter.js";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const handleClick = () => {
-    setCount((count) => {
-      return count + 1;
-    });
-  };
-  useEffect(() => {
-    console.log(`updated count: ${count}`)
-  }, [count])
 
   return (
-    <div>
-      <button onClick={handleClick}>Increment</button>
-      <p>Count: {count}</p>
-    </div>
+   <>
+    <h1>Counters</h1>
+    <section>
+      <Counter name="One" />
+      <Counter name="Two" />
+    </section>
+   </>
   );
 }
---
+++ src/Counter.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export function Counter(props) {
+    const [numOfClicks, setNumOfClicks] = useState(0);
+
+    const handleClick = () => {
+        setNumOfClicks(numOfClicks + 1)
+    }
+
+    return (
+        <article>
+            <h2>Counter {props.name}</h2>
+            <p>You clicked {numOfClicks} times</p>
+            <button onClick={handleClick}>Increment</button>
+        </article>
+    )
+}
