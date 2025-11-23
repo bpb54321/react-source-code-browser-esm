b5af1ad9e9c8aaab69d0cf31bc5756a5b644c643
Brian Blosser <bpb54321@gmail.com>
2025-11-23 12:25:14 -0500

    Add some useEffect calls in Counter.

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function Counter(props) {
     const [numOfClicks, setNumOfClicks] = useState(0);
     const [firstName, setFirstName] = useState("Brian");
     const [lastName, setLastName] = useState("Blosser");
+
+    useEffect(function firstEffectCreate() {
+        console.log(`firstEffect numOfClicks: ${numOfClicks}`)
+    }, [numOfClicks])
+    useEffect(function secondEffectCreate() {
+        console.log(`secondEffect numOfClicks: ${numOfClicks}`)
+    }, [numOfClicks])
 
     const handleClick = () => {
         setNumOfClicks(numOfClicks + 1)
